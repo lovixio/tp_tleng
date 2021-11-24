684f98bdb9e97534b9ba287fea8edcc970c13f36
Dario <diort.2018@gmail.com>
2021-11-23 21:26:03 -0300

    Agrego el enrroque y los comentarios cada uno con su nivel

--- parser_meta.py
+++ parser_meta.py
@@ -2,8 +2,6 @@
 import ply.lex as lex
 from ply.lex import LexError, StringTypes
 import ply.yacc as yacc
-import os
-os.system('color') # Sirve para que se vean los colorsitos del test en windows
 from funciones_auxiliares import *
 
 # Tokens
@@ -13,14 +11,15 @@ tokens = (
     'LCORCHETE' , 'RCORCHETE',
     'COMILLA', 'ESPACIO' , 'newline',
     'MENOS', 'NUM', 'PUNTO', 'CASILLA', 'PIEZA' , 'SLASH',
-    'MAS', 'PREGUNTA', 'EXCLAMACION', 'EQUIS'
+    'MAS', 'PREGUNTA', 'EXCLAMACION', 'EQUIS', 'O',
+    'LPAREN', 'RPAREN', 'LLLAVE', 'RLLAVE'
 )
 
 t_LCORCHETE = r'\['
 t_RCORCHETE = r'\]'
 
 #CARACTERES se refiere a todos los caracteres que solo se usen para la metaData y comentarios
-t_CARACTERES = r'[^\s\[\]\"\d+\.\-a-hPNBRQK\/\+\?!x]+' 
+t_CARACTERES = r'[^\s\[\]\"\d+\.\-a-hPNBRQK\/\+\?!xO\(\)\{\}]+' 
 t_COMILLA = r'\"'
 t_ESPACIO = r'\ '
 t_MENOS = r'-'
@@ -33,8 +32,16 @@ t_MAS = r'\+'
 t_PREGUNTA = r'\?'
 t_EXCLAMACION = r'!'
 t_EQUIS = r'x'
+t_O = r'O'
+t_LPAREN = r'\('
+t_RPAREN = r'\)'
+t_LLLAVE = r'\{'
+t_RLLAVE = r'\}'
 
-
+class Comentario:
+    def __init__(self, mensaje, nivel):
+        self.nivel = nivel
+        self.mensaje = mensaje
 
 def t_newline(t):
     r'\n+'
@@ -60,26 +67,49 @@ def p_metadataSegment_MetaData(t):
                        | partida'''
 
 def p_metadata_renglonMetaData(t):
-    '''metadata : string ESPACIO COMILLA string COMILLA'''
+    '''metadata : stringMeta1 ESPACIO COMILLA stringMeta2 COMILLA'''
     print(t[1] + ' ' + '"' + t[4] + '"')
 
 #Para parsear las cosas escritas usamos una recursion sobre los distintos tokens que se pueden encontrar
-def p_string_escritoEnLaMetaData(t):
-    '''string : contenido string
-              | contenido Empty'''
+def p_stringMeta_escritoEnPrimerElementoMetadata(t):
+    '''stringMeta1 : contenidoMeta1 stringMeta1
+                   | contenidoMeta1 Empty'''
     t[0] = t[1] + t[2]
 
-def p_contenido_delString(t):
-    '''contenido : NUM
-                 | MENOS
-                 | CARACTERES
-                 | CASILLA
-                 | PIEZA
-                 | SLASH
-                 | MAS
-                 | PREGUNTA
-                 | EXCLAMACION
-                 | EQUIS'''
+def p_contenidoMeta1_delStringMeta1(t):
+    '''contenidoMeta1 : caracteresnormales
+                      | LPAREN
+                      | RPAREN
+                      | LLLAVE
+                      | RLLAVE'''
+    t[0] = t[1]
+
+def p_stringMeta2_escritoEnSegundoElementoMetadata(t):
+    '''stringMeta2 : contenidoMeta2 stringMeta2
+                   | contenidoMeta2 Empty'''
+    t[0] = t[1] + t[2]
+
+def p_contenidoMeta2_delStringMeta2(t):
+    '''contenidoMeta2 : caracteresnormales
+                      | ESPACIO
+                      | LPAREN
+                      | RPAREN
+                      | LLLAVE
+                      | RLLAVE'''
+    t[0] = t[1]
+
+def p_caracteresnormales(t):
+    '''caracteresnormales : NUM
+                        | MENOS
+                        | CARACTERES
+                        | CASILLA
+                        | PIEZA
+                        | SLASH
+                        | MAS
+                        | PREGUNTA
+                        | EXCLAMACION
+                        | EQUIS
+                        | O '''
     t[0] = t[1]
 
 #una partida empieza con una jugada y una siguiente jugada (la cual puede ser el score, indicando el final de la partida)
@@ -89,12 +119,20 @@ def p_partida(t):
         p_error(t)
 
 def p_jugada(t):
-    '''jugada : NUM PUNTO ESPACIO movimiento movimiento '''
-    print("Jugada numero", t[1], "se jugo:", t[4], t[5])
-    
+    '''jugada : NUM PUNTO ESPACIO movimiento comentario movimiento comentario '''
+    print("Jugada numero", t[1], "se jugo:", t[4], t[6])
+
     t[0] = int(t[1])
     if t[0] == 0:
         p_error(t)
+
+def p_sigjugada(t):
+    '''sigjugada : jugada sigjugada 
+                 | score'''
+    if t[1]!=0 and not (t[2] == t[1]+1 or t[2] == 0):
+        p_error(t)
+    #pasamos el número de la jugada definida ahora como número de la siguiente jugada anterior para comparar con la anterior
+    t[0] = t[1]
 
 def p_movimiento(t):
     ''' movimiento : pieza casillas NUM mate calidad ESPACIO '''
@@ -103,6 +141,14 @@ def p_movimiento(t):
     numCasilla = int(t[3])
     if numCasilla == 0 or numCasilla >= 9:
         p_error(t)
+
+def p_movimiento_erroqueLargo(t):
+    '''movimiento : O MENOS O MENOS O ESPACIO'''
+    t[0] = t[1] + t[2] + t[3] + t[4] + t[5]
+
+def p_movimiento_erroqueCorto(t):
+    '''movimiento : O MENOS O ESPACIO'''
+    t[0] = t[1] + t[2] + t[3]
 
 def p_pieza(t):
     ''' pieza : PIEZA
@@ -113,7 +159,6 @@ def p_casillas_CapturaDestino(t):
     '''casillas : CASILLA'''
     t[0] = t[1]
 
-#aca hay un bug que no sabe diferenciar estos dos casos y piensa que el segundo movimiento en el .txt está mal    
 def p_casillas_OrigenCapturaDestino(t):
     '''casillas : origen captura CASILLA'''
     t[0] = t[1] + t[2] + t[3]
@@ -139,14 +184,33 @@ def p_mate(t):
             | MAS MAS
             | Empty '''
 
-def p_sigjugada(t):
-    '''sigjugada : jugada sigjugada 
-                 | score'''
-    if t[1]!=0 and not (t[2] == t[1]+1 or t[2] == 0):
-        p_error(t)
-    #pasamos el número de la jugada definida ahora como número de la siguiente jugada anterior para comparar con la anterior
-    t[0] = t[1]
-    
+def p_contenidoComent_delStringComent(t):
+    '''contenidoComent : caracteresnormales Empty
+                       | ESPACIO Empty
+                       | ESPACIO comentario'''
+    if t[2] == '':
+        t[0] = Comentario(t[1], 1)
+    else:
+
+        t[0] = Comentario(t[1] + t[2].mensaje, t[2].nivel + 1)
+
+def p_stringComent_escritoEnSComentarios(t):
+    '''stringComent : contenidoComent stringComent
+                    | contenidoComent Empty'''
+    if t[2] == '':
+        t[0] = t[1]
+    else:
+        t[0] = Comentario(t[2].mensaje + t[1].mensaje, t[2].nivel)
+
+def p_comentario(t):
+    ''' comentario : LPAREN stringComent RPAREN ESPACIO
+                   | LLLAVE stringComent RLLAVE ESPACIO
+                   | Empty Empty Empty Empty '''
+    if(t[1] == ''):
+        t[0] = Comentario('', 0)
+    else:
+        t[0] = Comentario(t[1] + t[2].mensaje + t[3] + t[4], t[2].nivel)
+
 def p_score_simplificado(t):
     '''score : NUM MENOS NUM'''
     t[0] = 0
@@ -216,6 +280,18 @@ testsToRun.append(('''[prueba "loca"]
 # Test 6: movimientos con capturas y con origenes
 testsToRun.append(('''[prueba "loca"]
 1. a4! Bxg5 2. Ka4 h3++ 3. Nbd7 N2d4 0-1''', 1))
+
+# Test 7: partida con enrroque corto y largo
+testsToRun.append(('''[prueba "loca"]
+1. a4! Bxg5 2. O-O h3++ 3. O-O-O N2d4 0-1''', 1))
+
+# Test 8: partida con parentesis y llaves en los strings y comentarios
+testsToRun.append(('''[prueba "loca"]
+1. a4! ( aisjdoaijdqoiwdj31124oqjd13jiqdjq ) Bxg5 { sakdasidasdasdi } 2. O-O h3++ 3. O-O-O { sdlajsid JUH775753DdffP-+KJ } N2d4 0-1''', 1))
+
+# Test 9: partida con comentarios dentro de comentarios
+testsToRun.append(('''[test9 "loca"]
+1. a4! ( aisjdoaijdqoiwdj31124oqjd13jiqdjq ) Bxg5 { sakdasidasdasdi (asdjas324jsadi3j qefi) } 2. O-O h3++ 3. O-O-O { sdlajsid JUH775753DdffP-+KJ {dqdoisajd8998 { odaoausd0} sds3 (lsaks iji   ksdi) } } N2d4 0-1''', 1))
 
 # Se puede descomentar una linea en runTest para que los test sean los paths
 # a los txt y no la cadena directa a parsear
